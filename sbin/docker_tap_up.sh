e4a21b440a74c342384f17563186c2bb6c5254e1
Marshkov Dmitry <d.marshkov@melsoft-games.com>
2019-04-26 10:54:47 +0300

    wip

--- sbin/docker_tap_up.sh
+++ sbin/docker_tap_up.sh
@@ -4,13 +4,19 @@ set -o nounset
 set -o errexit
 
 # Local and host tap interfaces
-localTapInterface=tap1
-hostTapInterface=eth1
+local_tap_interface=tap1
+host_tap_interface=eth1
 
 # Local and host gateway addresses
-localGateway='10.0.75.1/24'
-hostGateway='10.0.75.2'
+local_gateway="10.0.75.1/24"
+host_gateway="10.0.75.2"
+host_netmask="255.255.255.252"
+
+# Permit access to the tap device by user
+# TODO check if it's done
+# TODO restart docker if it's started and we need to change the owner
+sudo chown $USER /dev/$local_tap_interface
 
 # Startup local and host tuntap interfaces
-sudo ifconfig $localTapInterface $localGateway up
-docker run --rm --privileged --net=host --pid=host alpine ifconfig $hostTapInterface $hostGateway up
+sudo ifconfig $local_tap_interface $local_gateway up
+docker run --rm --privileged --net=host --pid=host alpine ifconfig $host_tap_interface $host_gateway netmask $host_netmask up
